912a54ce1872319c7f67edeb56ed1e4f196c9d05
mmdzov <mzov939@gmail.com>
2023-07-24 08:56:56 +0430

    Update

---
+++ examples/balance.js
@@ -0,0 +1,23 @@
+const Pmoa = require("pmoa");
+const { ProxyAdapter, SocksAdapter } = require("pmoa/src/proxy");
+const dotenv = require("dotenv");
+
+console.log(Pmoa);
+
+dotenv.config();
+
+const pmoa = new Pmoa(
+  {
+    AccountID: process.env.AccountID,
+    PassPhrase: process.env.PASSPHRASE,
+  },
+  {
+    proxy: new ProxyAdapter(new SocksAdapter(process.env.SOCKS)),
+  },
+);
+
+(async () => {
+  const result = await pmoa.balance();
+
+  console.log(result);
+})();
