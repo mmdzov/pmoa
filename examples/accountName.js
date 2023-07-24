639576da10fdb4a3b512cec2adc524d1441edcad
mmdzov <mzov939@gmail.com>
2023-07-24 09:03:02 +0430

    Update

---
+++ examples/accountName.js
@@ -0,0 +1,21 @@
+const Pmoa = require("pmoa");
+const { ProxyAdapter, SocksAdapter } = require("pmoa/src/proxy");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const pmoa = new Pmoa(
+  {
+    AccountID: process.env.AccountID,
+    PassPhrase: process.env.PASSPHRASE,
+  },
+  {
+    proxy: new ProxyAdapter(new SocksAdapter(process.env.SOCKS)),
+  },
+);
+
+(async () => {
+  const result = await pmoa.accountName("U41708332");
+
+  console.log(result);
+})();
